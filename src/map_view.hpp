fd9e9e2ddc9c4f4cfd0896ff83735c3223ea18e7
quendoris <quendoris@proton.me>
2026-08-19 20:43:00 +0000

    desktop: turn map view into live vector viewport

--- src/map_view.hpp
+++ src/map_view.hpp
@@ -1,12 +1,22 @@
 // SPDX-FileCopyrightText: 2026 quendoris
 // SPDX-License-Identifier: AGPL-3.0-only
+
 #pragma once
 
+#include "aeris/view/scene.hpp"
+#include "project_model.hpp"
+#include "scene_controller.hpp"
+
 #include <QPoint>
+#include <QPointF>
 #include <QWidget>
 
-#include <cstdint>
+#include <functional>
+#include <memory>
 #include <string>
+
+class QMouseEvent;
+class QWheelEvent;
 
 namespace aeris::desktop {
 
@@ -14,10 +24,21 @@ class MapView final : public QWidget {
     Q_OBJECT
 
 public:
+    using SceneRequestCallback = std::function<void(const view::SceneRequest&)>;
+
     explicit MapView(QWidget* parent = nullptr);
 
-    void set_project_summary(std::string project_uuid, std::uint64_t revision);
+    void set_project(
+        std::shared_ptr<const ProjectModel> model,
+        std::string project_uuid,
+        std::uint64_t revision);
     void clear_project();
+
+    void set_scene_request_callback(SceneRequestCallback callback);
+    void set_frame(RenderFrame frame);
+    void set_busy(bool busy);
+    void set_surface_mode(view::SurfaceMode mode);
+    [[nodiscard]] view::SurfaceMode surface_mode() const noexcept { return mode_; }
 
 protected:
     void paintEvent(QPaintEvent* event) override;
@@ -27,13 +48,25 @@ protected:
     void mouseReleaseEvent(QMouseEvent* event) override;
 
 private:
-    bool has_project_{false};
+    void request_scene(view::SceneQuality quality);
+
+    std::shared_ptr<const ProjectModel> model_;
     std::string project_uuid_;
-    std::uint64_t revision_{0};
-    double interaction_scale_{1.0};
-    QPoint interaction_offset_{};
+    std::uint64_t revision_{0U};
+    RenderFrame frame_{};
+    bool has_frame_{false};
+    bool busy_{false};
+    std::string frame_error_;
+
+    view::SurfaceMode mode_{view::SurfaceMode::globe};
+    double longitude_deg_{15.0};
+    double latitude_deg_{20.0};
+    double zoom_{1.0};
+    QPointF flat_pan_{};
+
     QPoint last_mouse_{};
     bool dragging_{false};
+    SceneRequestCallback scene_request_callback_;
 };
 
 }  // namespace aeris::desktop
